65a88420e498d26ecfeb169a31f765ff30278c55
Sena Kendali <sena.kendali@gmail.com>
2025-09-10 21:43:46 +0700

    Update app settings

--- config/app_settings.php
+++ config/app_settings.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    'data_source' => 'https://backend.cjmanajemen.co.id',
+    //'data_source' => 'https://backend.cjmanajemen.co.id',
+    'data_source' => 'https://backend.bmudigitalscoring.co.id',
     //'data_source' => 'http://127.0.0.1:8002',
     'site_name' => 'Senstech',
     'default_timezone' => 'Asia/Jakarta',
